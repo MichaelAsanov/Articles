53c57a8e1bf4168d39749fc045969f8f006ec952
Mickael <MA>
2020-01-16 01:56:47 +0300

    Add FillDB file; Change indent to 4

---
+++ 02 - FillDB.sql
@@ -0,0 +1,41 @@
+﻿BEGIN TRAN
+BEGIN TRY
+
+    INSERT INTO 
+        Article (Title)
+    VALUES('Математика для разработчиков')
+
+    DECLARE @mathArticleId INT = SCOPE_IDENTITY()
+
+    INSERT INTO
+        Tag ([Name])
+    VALUES('Математика')
+    
+    DECLARE @mathTagId INT = SCOPE_IDENTITY()
+
+    INSERT INTO
+        Tag ([Name])
+    VALUES('Разработка')
+    
+    DECLARE @devTagId INT = SCOPE_IDENTITY()
+    
+    INSERT INTO
+        Tag ([Name])
+    VALUES('Информатика')
+    
+    DECLARE @infTagId INT = SCOPE_IDENTITY()
+
+    
+        
+IF XACT_STATE() > 0 
+	COMMIT TRAN
+
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() != 0 
+		ROLLBACK TRAN
+	DECLARE @err varchar(MAX) = ERROR_MESSAGE()
+	DECLARE @line INT = ERROR_LINE()
+	
+	RAISERROR('Line: %d. Error: %s', 15, 1, @line, @err)
+END CATCH
