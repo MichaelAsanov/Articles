9a21cfa37b762f000326a77ce4ca0231b30a9a7b
Mickael <MA>
2020-01-16 02:19:01 +0300

    Fill Db file

--- 02 - FillDB.sql
+++ 02 - FillDB.sql
@@ -25,7 +25,53 @@ BEGIN TRY
     
     DECLARE @infTagId INT = SCOPE_IDENTITY()
 
+    INSERT INTO RlArticleToTag
+        (ArticleID,      TagID      )
+    VALUES
+        (@mathArticleId, @mathTagId ),
+        (@mathArticleId, @devTagId  ),
+        (@mathArticleId, @infTagId  )
+
+    INSERT INTO 
+        Article (Title)
+    VALUES('Алгоритмы для разработчиков')
+
+    DECLARE @algArticleId INT = SCOPE_IDENTITY()
+
+    INSERT INTO
+        Tag ([Name])
+    VALUES('Алгоритмы')
     
+    DECLARE @algTagId INT = SCOPE_IDENTITY()
+
+    INSERT INTO RlArticleToTag
+        (ArticleID,     TagID      )
+    VALUES
+        (@algArticleId, @algTagId  ),
+        (@algArticleId, @devTagId  ),
+        (@algArticleId, @infTagId  )
+
+    INSERT INTO
+        Article (Title)
+    VALUES('Что такое ОГАС')
+
+    DECLARE @ogasArticleId INT = SCOPE_IDENTITY()
+
+    INSERT INTO
+        Tag ([Name])
+    VALUES('Экономика')
+    
+    DECLARE @economicsTagId INT = SCOPE_IDENTITY()
+
+    INSERT INTO RlArticleToTag
+        (ArticleID,      TagID            )
+    VALUES
+        (@ogasArticleId, @infTagId        ),
+        (@ogasArticleId, @economicsTagId  )
+
+    INSERT INTO
+        Article (Title)
+    VALUES('Статья без тегов')
         
 IF XACT_STATE() > 0 
 	COMMIT TRAN
