5d9a588404b635dccbfe5aac5d53c82a457db840
Mickael <MA>
2020-01-16 03:31:23 +0300

    Добавил человеческие имена столбцов

--- 03 - SelectArtcileTitleToTagName.sql
+++ 03 - SelectArtcileTitleToTagName.sql
@@ -1,6 +1,7 @@
 ﻿USE Articles
 
-SELECT a.Title, t.[Name]
+SELECT a.Title AS 'Тема статьи'
+    , t.[Name] AS 'Тег'
     FROM Article a
         LEFT JOIN rlArticleToTag rl ON a.ArticleID = rl.ArticleID
         LEFT JOIN Tag t ON rl.TagID = t.TagID
